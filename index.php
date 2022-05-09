005af384c4eead61bb0094297a1be5cbfc3118bc
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-05-09 15:59:17 +0200

    aggiunto a index

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
     include_once __DIR__ . "/classes/Croccantini.php";
-    include_once __DIR__ . "/classes/Payments.php";
+    include_once __DIR__ . "/classes/CreditCard.php";
 
+$royalCanin = new Croccantini('royal canin', 'carne', 'cibo per cani', 'croccantini', 150, 10);
+var_dump($royalCanin);
+$payment = new CreditCard('Visa', false, 20, 'croccantini', 200000, 15);
 
-
+echo $payment->setDeal();
+echo $payment->isFell();
 
 ?>
 
@@ -16,6 +20,38 @@
     <title>OOP 2</title>
 </head>
 <body>
-    
+    <h4>
+        il prodotto selezionato è: <?php echo $royalCanin->getName()?>
+    </h4>
+    <ul>
+        <li>la marca del prodotto è:
+            <?php echo $royalCanin->getGenre()?>
+        </li>
+        <li>il tipo di mangime è: 
+            <?php echo $royalCanin->getTypeOfAnimals()?>
+        </li>
+        <li>il tipo di prodotto è: 
+            <?php echo $royalCanin->getType()?>
+        </li>
+        <li>il prezzo intero del prodotto è: 
+            <?php echo $royalCanin->getPrice()?>
+        </li>
+        <li>il peso del prodotto è:
+            <?php echo $royalCanin->getSize()?>
+        </li>
+    </ul>
+    <h4>
+        l'utente è registrato? <?php echo $payment->isRegistered()?>
+    </h4>
+    <h5>
+        il prezzo finale è:
+        <?php 
+        echo $payment->setDeal();
+        ?>
+    </h5>
+    <h2>
+        <?php 
+        echo $payment->isFell()?>
+    </h2>
 </body>
 </html>
